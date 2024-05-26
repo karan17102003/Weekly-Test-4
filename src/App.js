2230b1cef89601e7c3313d425806c029803f220b
karan17102003 <karanmehra17102003@gmail.com>
2024-05-26 23:19:16 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Employees from './employees';
+
+function App() {
+  return (
+    <div className="App">
+      <Employees/>
+    </div>
+  );
+}
+
+export default App;
